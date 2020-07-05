14a65a91eeebe6350e592eed432dd3ab51a25425
Nir Argaman <nargaman@redhat.com>
2020-07-05 15:08:30 +0300

    Add machine (for controlplane) and machineset (for compute) to KubeVirt provider
    * Those configurations are used in the "manifests" stage
    * Use providerSpec (declared in cluster_api_provider_kubevirt) inside the configurations

--- pkg/asset/machines/worker.go
+++ pkg/asset/machines/worker.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 
 	"github.com/ghodss/yaml"
+	kubevirtproviderapi "github.com/kubevirt/cluster-api-provider-kubevirt/pkg/apis"
+	kubevirtprovider "github.com/kubevirt/cluster-api-provider-kubevirt/pkg/apis/kubevirtprovider/v1alpha1"
 	baremetalapi "github.com/metal3-io/cluster-api-provider-baremetal/pkg/apis"
 	baremetalprovider "github.com/metal3-io/cluster-api-provider-baremetal/pkg/apis/baremetal/v1alpha1"
 	gcpapi "github.com/openshift/cluster-api-provider-gcp/pkg/apis"
@@ -38,6 +40,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/machines/azure"
 	"github.com/openshift/installer/pkg/asset/machines/baremetal"
 	"github.com/openshift/installer/pkg/asset/machines/gcp"
+	"github.com/openshift/installer/pkg/asset/machines/kubevirt"
 	"github.com/openshift/installer/pkg/asset/machines/libvirt"
 	"github.com/openshift/installer/pkg/asset/machines/machineconfig"
 	"github.com/openshift/installer/pkg/asset/machines/openstack"
@@ -52,6 +55,7 @@ import (
 	azuredefaults "github.com/openshift/installer/pkg/types/azure/defaults"
 	baremetaltypes "github.com/openshift/installer/pkg/types/baremetal"
 	gcptypes "github.com/openshift/installer/pkg/types/gcp"
+	kubevirttypes "github.com/openshift/installer/pkg/types/kubevirt"
 	libvirttypes "github.com/openshift/installer/pkg/types/libvirt"
 	nonetypes "github.com/openshift/installer/pkg/types/none"
 	openstacktypes "github.com/openshift/installer/pkg/types/openstack"
@@ -138,6 +142,14 @@ func defaultVSphereMachinePoolPlatform() vspheretypes.MachinePool {
 		OSDisk: vspheretypes.OSDisk{
 			DiskSizeGB: 120,
 		},
+	}
+}
+
+func defaultKubevirtMachinePoolPlatform() kubevirttypes.MachinePool {
+	return kubevirttypes.MachinePool{
+		CPU:         4,
+		Memory:      "16348M",
+		StorageSize: "35Gi",
 	}
 }
 
@@ -388,6 +400,21 @@ func (w *Worker) Generate(dependencies asset.Parents) error {
 			for _, set := range sets {
 				machineSets = append(machineSets, set)
 			}
+		case kubevirttypes.Name:
+			mpool := defaultKubevirtMachinePoolPlatform()
+			mpool.Set(ic.Platform.Kubevirt.DefaultMachinePlatform)
+			mpool.Set(pool.Platform.Kubevirt)
+			pool.Platform.Kubevirt = &mpool
+
+			imageName, _ := rhcosutils.GenerateOpenStackImageName(string(*rhcosImage), clusterID.InfraID)
+
+			sets, err := kubevirt.MachineSets(clusterID.InfraID, ic, &pool, imageName, "worker", "worker-user-data")
+			if err != nil {
+				return errors.Wrap(err, "failed to create worker machine objects for kubevirt provider")
+			}
+			for _, set := range sets {
+				machineSets = append(machineSets, set)
+			}
 		case nonetypes.Name:
 		default:
 			return fmt.Errorf("invalid Platform")
@@ -472,6 +499,7 @@ func (w *Worker) MachineSets() ([]machineapi.MachineSet, error) {
 	openstackapi.AddToScheme(scheme)
 	ovirtproviderapi.AddToScheme(scheme)
 	vsphereproviderapi.AddToScheme(scheme)
+	kubevirtproviderapi.AddToScheme(scheme)
 	decoder := serializer.NewCodecFactory(scheme).UniversalDecoder(
 		awsprovider.SchemeGroupVersion,
 		azureprovider.SchemeGroupVersion,
@@ -481,6 +509,7 @@ func (w *Worker) MachineSets() ([]machineapi.MachineSet, error) {
 		openstackprovider.SchemeGroupVersion,
 		ovirtprovider.SchemeGroupVersion,
 		vsphereprovider.SchemeGroupVersion,
+		kubevirtprovider.SchemeGroupVersion,
 	)
 
 	machineSets := []machineapi.MachineSet{}
