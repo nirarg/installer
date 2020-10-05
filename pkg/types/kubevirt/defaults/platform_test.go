83bd910401b3c00a6dbd4d3cf0500cf7ad390ee9
Nir Argaman <nargaman@redhat.com>
2020-10-05 10:16:57 +0300

    Add kubevirt platform defaults

---
+++ pkg/types/kubevirt/defaults/platform_test.go
@@ -0,0 +1,61 @@
+package defaults
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/kubevirt"
+)
+
+func defaultInstallConfig() *types.InstallConfig {
+	ic := &types.InstallConfig{
+		ControlPlane: &types.MachinePool{
+			Name: "master",
+		},
+		Compute: []types.MachinePool{
+			{
+				Name: "worker",
+			},
+		},
+	}
+	return ic
+}
+
+func expectedInstallConfig() *types.InstallConfig {
+	ic := defaultInstallConfig()
+	ic.ControlPlane.Platform.Kubevirt = &kubevirt.MachinePool{
+		CPU:         8,
+		Memory:      "16G",
+		StorageSize: "120Gi",
+	}
+	ic.Compute[0].Platform = types.MachinePoolPlatform{
+		Kubevirt: &kubevirt.MachinePool{
+			CPU:         4,
+			Memory:      "10G",
+			StorageSize: "120Gi",
+		},
+	}
+	return ic
+}
+
+func TestSetPlatformDefaults(t *testing.T) {
+	cases := []struct {
+		name     string
+		ic       *types.InstallConfig
+		expected *types.InstallConfig
+	}{
+		{
+			name:     "happy_fllow",
+			ic:       defaultInstallConfig(),
+			expected: expectedInstallConfig(),
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			SetPlatformDefaults(tc.ic.Platform.Kubevirt, tc.ic.ControlPlane, tc.ic.Compute)
+			assert.Equal(t, tc.expected, tc.ic, "unexpected InstallConfig")
+		})
+	}
+}
