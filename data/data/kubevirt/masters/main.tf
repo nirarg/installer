627d9a35abb4e98d65e2f78a6cc34342470cc4a2
Nir Argaman <nargaman@redhat.com>
2020-10-22 13:35:45 +0300

    Support antiaffinity

--- data/data/kubevirt/masters/main.tf
+++ data/data/kubevirt/masters/main.tf
@@ -11,20 +11,28 @@ resource "kubernetes_secret" "master_ignition" {
   }
 }
 
+locals {
+  anti_affinity_label = {
+    "anti-affinity-tag-${var.cluster_id}" = "master"
+  }
+}
+
 resource "kubevirt_virtual_machine" "master_vm" {
   wait = true
 
-  count                = var.master_count
-  name                 = "${var.cluster_id}-master-${count.index}"
-  namespace            = var.namespace
-  labels               = var.labels
-  storage_size         = var.storage
-  memory               = var.memory
-  cpu                  = var.cpu
-  storage_class_name   = var.storage_class
-  network_name         = var.network_name
-  access_mode          = var.pv_access_mode
-  ignition_secret_name = kubernetes_secret.master_ignition[count.index].metadata[0].name
-  image_url            = var.image_url
+  count                      = var.master_count
+  name                       = "${var.cluster_id}-master-${count.index}"
+  namespace                  = var.namespace
+  labels                     = merge(var.labels, local.anti_affinity_label)
+  storage_size               = var.storage
+  memory                     = var.memory
+  cpu                        = var.cpu
+  storage_class_name         = var.storage_class
+  network_name               = var.network_name
+  access_mode                = var.pv_access_mode
+  ignition_secret_name       = kubernetes_secret.master_ignition[count.index].metadata[0].name
+  image_url                  = var.image_url
+  anti_affinity_match_labels = local.anti_affinity_label
+  anti_affinity_topology_key = "kubernetes.io/hostname"
   // pvc_name = var.pvc_name
 }
